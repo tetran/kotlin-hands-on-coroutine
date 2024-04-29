fba3788677b5b66ebec760dd0478520302bc7c27
Koichi Kaneshige <coarse.ground@gmail.com>
2024-04-29 15:37:15 +0900

    Blocking requests

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -3,8 +3,6 @@ package tasks
 import contributors.User
 
 /*
-TODO: Write aggregation code.
-
  In the initial list each user is present several times, once for each
  repository he or she contributed to.
  Merge duplications: each user should be present only once in the resulting list
@@ -15,4 +13,18 @@ TODO: Write aggregation code.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
 fun List<User>.aggregate(): List<User> =
-    this
+    groupBy { it.login }
+        .map { (login, group) -> User(login, group.sumOf { it.contributions })}
+        .sortedByDescending { it.contributions }
+
+// // My solution
+//fun List<User>.aggregate(): List<User> {
+//    val map: MutableMap<String, Int> = mutableMapOf()
+//    for (user in this) {
+//        map[user.login] = (map[user.login] ?: 0) + user.contributions
+//    }
+//    return map.entries.stream()
+//        .map { (k, v) -> User(k, v) }
+//        .sorted { u1, u2 -> u2.contributions - u1.contributions }
+//        .toList()
+//}
