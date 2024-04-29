83800dc2ba14122df5dd6f8a6e521060eb505832
Koichi Kaneshige <coarse.ground@gmail.com>
2024-04-29 18:38:48 +0900

    Showing progress

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,17 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    val allUsers = mutableListOf<User>()
+    for ((i, repo) in repos.withIndex()) {
+        val users = service.getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        allUsers += users
+        updateResults(allUsers.aggregate(), i == repos.size)
+    }
 }
